ccdd259625a45ced87fe20428cb0cbc528e86324
ea <64137287+emreaksel@users.noreply.github.com>
2022-03-02 23:50:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -68,8 +68,8 @@
         }
         //----------------------
         function seekTo() {
-                const seekSlider = document.getElementsByClassName('seek_slider');
-            var seekto = parseInt(curr_track.duration * (seekSlider.value / 100)); //bu satırda jquery kullnınca hata aldık
+            const seekSlider = document.getElementsByClassName('seek_slider');
+            var seekto = Math.floor(curr_track.duration * (Math.floor($(".seek_slider").value) / 100)); //bu satırda jquery kullnınca hata aldık
             curr_track.currentTime = seekto;
         }
         //----------------------
