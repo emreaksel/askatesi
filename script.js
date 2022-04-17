3de2b7c152d08541cef7f2b63fd089be698454b2
ea <64137287+emreaksel@users.noreply.github.com>
2022-04-17 15:20:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
         //merhabalar :)
-        konsola_yaz("Debug", ' Denemeler: ' + 64);
+        konsola_yaz("Debug", ' Denemeler: ' + 65);
 
         var track_index = 0;
         var isPlaying = false;
@@ -181,7 +181,7 @@
           if (parselle.includes("?")) {
               console.log("paylaşılan bir parça okundu: " + "true");
             parselle=parselle.replace('https://atesiask.netlify.app/?', '');
-            var bilgi = parselle.split("\");
+            var bilgi = parselle.split("--");
                                          
             var url="https://www.mediafire.com/"+bilgi[0]+".mp3";
             var ad=bilgi[1];
